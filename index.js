95c62a245ee60d400d3fbc3502f0eda2e23e1f42
chiranthkothari <chiranthkothari96@gmail.com>
2022-03-26 15:10:08 +0530

    Removed cron

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
-const rp = require('request-promise');
-const $ = require('cheerio');
-var CronJob = require('cron').CronJob;
-const { Telegraf } = require('telegraf');
+const rp = require("request-promise");
+const $ = require("cheerio");
+const { Telegraf } = require("telegraf");
 
-const url = 'https://tamilblasters.buzz/';
+const url = "https://tamilblasters.buzz/";
 const bot = new Telegraf(process.env.BOT_ID);
 
-bot.start((ctx) => ctx.reply('Welcome'));
+//start handler
+bot.start((ctx) => {
+  ctx.reply(`
+Send the below commands to get latest movie releases on tamilblasters torrent site:
 
-var job = new CronJob('* */3 * * *', function () {
+1. /list10 To get latest 10 releases
+2. /list15 To get latest 15 releases 
+  `);
+});
+
+bot.command(["list10", "List10"], (ctx) => {
+  dataFetch(ctx, 10);
+});
+
+bot.command(["list15", "List15"], (ctx) => {
+  dataFetch(ctx, 15);
+});
+
+function dataFetch(ctx, listLength) {
   rp(url)
     .then(function (html) {
       const cheerioData = $.load(html);
-      const data = cheerioData('p:nth-of-type(n+3)').text().split(/\r?\n/);
+      const data = cheerioData("p:nth-of-type(n+3)").text().split(/\r?\n/);
       const cleanedData = [];
       data.map((data) => {
-        if (data.trim().length < 5) {
+        if (data.trim().length < 2) {
           return;
         } else {
-          cleanedData.push(data.trim().substring(0, data.indexOf('-') - 1));
+          const trimmedData = data.trim().substring(0, data.indexOf("-") - 1);
+          if (trimmedData.length > 5) {
+            cleanedData.push(trimmedData);
+          }
         }
       });
-      let message = `Latest 10 releases:\n\n`;
-      for (i = 0; i < 10; i++) {
+      let message = `Here is the list of latest ${listLength} releases in tamilblasters website:\n\n`;
+      for (i = 0; i < listLength; i++) {
         const singleData = `${i + 1}. ${cleanedData[i]}\n\n`;
         message += singleData;
       }
-      message += 'This data will be fetched once in every 3 hours';
-      bot.telegram.sendMessage('-1001702762453', message);
+      message += "Data fetched by @tamilmvtrackerbot";
+      ctx.reply(message);
     })
     .catch(function (err) {
       console.log(err);
     });
-});
-
-job.start()
+}
 
 bot.launch();
